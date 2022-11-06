4e03bfbdaadbff247e633e6769e2214fd0901382
David Howe <90102914+DavidHoweOTU@users.noreply.github.com>
2022-11-06 17:02:04 -0500

    Update home_page.dart

--- weather_app/lib/home_page.dart
+++ weather_app/lib/home_page.dart
@@ -15,8 +15,86 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    List<Widget> page = _generateHomePage();
     return Scaffold(
-      body: Text("Home"),
+      body: ListView.separated(
+        separatorBuilder: (context, index) {
+          return const SizedBox(height: 8);
+        },
+        itemCount: page.length,
+        itemBuilder: (context, index) {
+          return page[index];
+        }
+      ),
     );
+  }
+
+  _generateHomePage() {
+    return [
+      // TODO: Stack image of weather type?
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Text(
+            "90""°",
+            style: TextStyle(
+                fontSize: 80
+            ),
+          ),
+          Icon(
+            Icons.sunny,
+            size: 50,
+            color: Colors.yellow,
+          ),
+        ],
+      ),
+      const Center(child: Text("Feels like 80°")),
+      const Center(child: Text("High: 100°      Low: 40°")),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text("5 PM"),
+              Icon(Icons.sunny),
+              Text("90°"),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text("5 PM"),
+              Icon(Icons.sunny),
+              Text("90°"),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text("5 PM"),
+              Icon(Icons.sunny),
+              Text("90°"),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text("5 PM"),
+              Icon(Icons.sunny),
+              Text("90°"),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Text("5 PM"),
+              Icon(Icons.sunny),
+              Text("90°"),
+            ],
+          ),
+        ],
+      ),
+    ];
   }
 }
