48824d77574c0a460ffc505657ec512626d20383
David Howe <90102914+DavidHoweOTU@users.noreply.github.com>
2022-11-08 20:22:54 -0500

    Update more_page.dart
    The more page will now show icons and text for each selected setting

--- weather_app/lib/more_page.dart
+++ weather_app/lib/more_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/icon_reference.dart';
+import 'package:weather_app/settings_page.dart';
+import 'package:weather_app/weather_from_url.dart';
+
+import 'Weather.dart';
 
 // TODO
 // Average For Each Weather Info
@@ -28,7 +33,38 @@ class _MorePageState extends State<MorePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("More"),
+      // body: Text("More"),
+      body: FutureBuilder(
+          future: getWeather(context),
+          builder: (context, snapshot) {
+            print("Recieved weather data");
+            return snapshot.hasData
+            ? ListView.separated(
+              itemCount: 13,
+              itemBuilder: (content, index) {
+                return index != 0 && settings.isChecked[index] == false
+                ? Container()
+                : Row(
+                  children: [
+                    Icon(
+                      IconReference.moreIcons[index],
+                      size: 30,
+                    ),
+                    Text(
+                      settings.settingNames[index],
+                      style: const TextStyle(fontSize: 30),
+                    ),
+                  ],
+                );
+              }, separatorBuilder: (BuildContext context, int index)
+                    =>  index != 0 && settings.isChecked[index] == false
+                        ? Container()
+                        : const Divider(),
+            )
+            : const Center(child: CircularProgressIndicator());
+          }
+      ),
     );
   }
 }
+
