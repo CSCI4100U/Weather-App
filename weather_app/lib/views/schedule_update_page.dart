6baf5c23c176925406cc3922f48c99205cf6dd54
Alden Chan <aldenchan1213@gmail.com>
2022-11-11 21:39:18 -0500

    Added appbar button in the weather updates page to cancel all ongoing weather updates

--- weather_app/lib/views/schedule_update_page.dart
+++ weather_app/lib/views/schedule_update_page.dart
@@ -9,7 +9,10 @@ import "package:timezone/timezone.dart" as tz;
 import '../models/weather_from_url.dart';
 
 class ScheduleUpdatePage extends StatefulWidget {
-  const ScheduleUpdatePage({Key? key}) : super(key: key);
+  ScheduleUpdatePage({Key? key, this.flutterLocalNotificationsPlugin, this.platformChannelInfo}) : super(key: key);
+
+  FlutterLocalNotificationsPlugin? flutterLocalNotificationsPlugin;
+  NotificationDetails? platformChannelInfo;
 
   @override
   State<ScheduleUpdatePage> createState() => _ScheduleUpdatePageState();
@@ -31,7 +34,7 @@ class _ScheduleUpdatePageState extends State<ScheduleUpdatePage> {
   TimeOfDay? setTime;
   String notificationType = "Weekly";
 
-  var _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  var _flutterLocalNotificationsPlugin;
   NotificationDetails? _platformChannelInfo;
 
   @override
@@ -39,51 +42,9 @@ class _ScheduleUpdatePageState extends State<ScheduleUpdatePage> {
     super.initState();
     dropdownValue = weekdays[DateTime.now().weekday-1];
     setTime = TimeOfDay.now();
+    _flutterLocalNotificationsPlugin = widget.flutterLocalNotificationsPlugin;
+    _platformChannelInfo = widget.platformChannelInfo;
     tz.initializeTimeZones();
-    preparePlatformChannelInfo();
-  }
-
-  void preparePlatformChannelInfo(){
-    if (Platform.isIOS){
-      _flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation
-      <IOSFlutterLocalNotificationsPlugin>()!.requestPermissions(
-          alert: false,
-          badge: true,
-          sound: true
-      );
-    }
-
-    var initializationSettingsAndroid = AndroidInitializationSettings("mipmap/ic_launcher");
-    var initializationSettingsIOS = DarwinInitializationSettings(
-      onDidReceiveLocalNotification: (int id, String? title, String? body, String? payload){
-        return null;
-      },
-    );
-
-    var initializationSettings = InitializationSettings(
-        android: initializationSettingsAndroid,
-        iOS: initializationSettingsIOS
-    );
-
-    _flutterLocalNotificationsPlugin.initialize(initializationSettings,
-      onDidReceiveNotificationResponse: onDidReceiveNotificationResponse,
-    );
-
-    var androidChannelInfo = AndroidNotificationDetails(
-        "WeatherUpdate", "Weather Notifications", channelDescription: "The Weather Update Is Set"
-    );
-
-    var iosChannelInfo = DarwinNotificationDetails();
-    _platformChannelInfo = NotificationDetails(
-        android: androidChannelInfo,
-        iOS: iosChannelInfo
-    );
-  }
-
-  Future onDidReceiveNotificationResponse(NotificationResponse notificationResponse) async{
-    if (notificationResponse != null){
-      print("onDidReceiveNotificationResponse::payload = ${notificationResponse.payload}");
-    }
   }
 
   @override
@@ -91,16 +52,33 @@ class _ScheduleUpdatePageState extends State<ScheduleUpdatePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Weather App"),
+        actions: [
+          IconButton(
+              tooltip: "Cancel All Ongoing Weather Updates",
+              onPressed: (){
+                _flutterLocalNotificationsPlugin.cancelAll();
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                        content: Text("Cancelled Ongoing Notifications")
+                    )
+                );
+              },
+              icon: Icon(Icons.stop)
+          )
+        ],
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-              const Text("Select When To Show Daily Weather Updates",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold
+              Container(
+                padding: const EdgeInsets.all(5),
+                child: const Text("Schedule A Periodic Weather Update Every",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                  ),
                 ),
               ),
               Row(
@@ -224,35 +202,39 @@ class _ScheduleUpdatePageState extends State<ScheduleUpdatePage> {
     // TODO Background Task For Fetching Real Time Weather Information
     // // If Daily Weather Update
     // if (notificationType == "Daily"){
-      var information = await weatherFromUrl(
-          "${generateUrl(43.90, -78.86)}"
-          "&start_date=${DateTime.now().toIso8601String().substring(0, 10)}"
-          "&end_date=${DateTime.now().toIso8601String().substring(0, 10)}"
-      ).then((information) async{
-        if (information.runtimeType == Weather){
-          return _flutterLocalNotificationsPlugin.zonedSchedule(
-            0,
-            "Weather Update",
-            " It's ${(information as Weather).temperatures![0].toString()}°C Right Now",
-            tz.TZDateTime(
-                tz.getLocation("Canada/Eastern"),
-                tz.TZDateTime.now(tz.getLocation("Canada/Eastern")).year,
-                tz.TZDateTime.now(tz.getLocation("Canada/Eastern")).month,
-                tz.TZDateTime.now(tz.getLocation("Canada/Eastern")).day,
-                setTime!.hour,
-                setTime!.minute
-            ),
-            _platformChannelInfo!,
-            androidAllowWhileIdle: true,
-            matchDateTimeComponents: DateTimeComponents.time,
-            uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime,
-          );
-        }
-        else{
-          ScaffoldMessenger.of(context).showSnackBar(information as SnackBar);
-        }
-      });
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Scheduled Notification")));
+      try {
+        var information = await weatherFromUrl(
+            "${generateUrl(43.90, -78.86)}"
+            "&start_date=${DateTime.now().toIso8601String().substring(0, 10)}"
+            "&end_date=${DateTime.now().toIso8601String().substring(0, 10)}"
+        ).then((information) async{
+          if (information.runtimeType == Weather){
+            _flutterLocalNotificationsPlugin.zonedSchedule(
+              0,
+              "Weather Update",
+              " It's ${(information as Weather).temperatures![0].toString()}°C Right Now",
+              tz.TZDateTime(
+                  tz.getLocation("Canada/Eastern"),
+                  tz.TZDateTime.now(tz.getLocation("Canada/Eastern")).year,
+                  tz.TZDateTime.now(tz.getLocation("Canada/Eastern")).month,
+                  tz.TZDateTime.now(tz.getLocation("Canada/Eastern")).day,
+                  setTime!.hour,
+                  setTime!.minute
+              ),
+              _platformChannelInfo!,
+              androidAllowWhileIdle: true,
+              matchDateTimeComponents: DateTimeComponents.time,
+              uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime,
+            );
+            Navigator.pop(context, "Scheduled Notification");
+          }
+          else{
+            Navigator.pop(context, "An Error Occured Scheduling Your Notification");
+          }
+        });
+      } catch (e) {
+        Navigator.pop(context, "An Error Occured Scheduling Your Notification");
+      }
     // }
     // // If Weekly Weather Update
     // else{
--- weather_app/lib/views/tools_page.dart
+++ weather_app/lib/views/tools_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:weather_app/views/schedule_update_page.dart';
+import 'dart:io';
 
 class ToolsPage extends StatefulWidget {
   const ToolsPage({Key? key}) : super(key: key);
@@ -9,19 +11,73 @@ class ToolsPage extends StatefulWidget {
 }
 
 class _ToolsPageState extends State<ToolsPage> {
+  var _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  NotificationDetails? _platformChannelInfo;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (Platform.isIOS){
+      _flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation
+      <IOSFlutterLocalNotificationsPlugin>()!.requestPermissions(
+          alert: false,
+          badge: true,
+          sound: true
+      );
+    }
+
+    var initializationSettingsAndroid = AndroidInitializationSettings("mipmap/ic_launcher");
+    var initializationSettingsIOS = DarwinInitializationSettings(
+      onDidReceiveLocalNotification: (int id, String? title, String? body, String? payload){
+        return null;
+      },
+    );
+
+    var initializationSettings = InitializationSettings(
+        android: initializationSettingsAndroid,
+        iOS: initializationSettingsIOS
+    );
+
+    _flutterLocalNotificationsPlugin.initialize(initializationSettings,
+      onDidReceiveNotificationResponse: onDidReceiveNotificationResponse,
+    );
+
+    var androidChannelInfo = AndroidNotificationDetails(
+        "WeatherUpdate", "Weather Notifications", channelDescription: "The Weather Update Is Set"
+    );
+
+    var iosChannelInfo = DarwinNotificationDetails();
+    _platformChannelInfo = NotificationDetails(
+        android: androidChannelInfo,
+        iOS: iosChannelInfo
+    );
+  }
+
+  Future onDidReceiveNotificationResponse(NotificationResponse notificationResponse) async{
+    if (notificationResponse != null){
+      print("onDidReceiveNotificationResponse::payload = ${notificationResponse.payload}");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-            onPressed: (){
-              Navigator.of(context).push(
+            onPressed: () async{
+              var status = await Navigator.of(context).push(
                   MaterialPageRoute(
-                      builder: (context) => const ScheduleUpdatePage()
+                      builder: (context) => ScheduleUpdatePage(
+                        flutterLocalNotificationsPlugin: _flutterLocalNotificationsPlugin,
+                        platformChannelInfo: _platformChannelInfo,
+                      )
                   )
               );
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(status as String)));
             },
-            child: const Text("Schedule Weather Updates")),
+            child: const Text("Manage Weather Updates")
+        ),
       ),
     );
   }
